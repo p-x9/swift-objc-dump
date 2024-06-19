3c9f82c8b5c5945e45a71120dd875c8826d57e49
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-06-19 18:06:00 +0900

    format

--- Package.swift
+++ Package.swift
@@ -1,23 +1,22 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "ObjCDump",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ObjCDump",
-            targets: ["ObjCDump"]),
+            targets: ["ObjCDump"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ObjCDump"),
+            name: "ObjCDump"
+        ),
         .testTarget(
             name: "ObjCDumpTests",
-            dependencies: ["ObjCDump"]),
+            dependencies: ["ObjCDump"]
+        ),
     ]
 )
