cea80c618aa222a586439c82df1e7c53bdfeb588
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-06-23 15:10:42 +0900

    Implemented model for bjc ivar

---
+++ Sources/ObjCDump/Model/ObjCIvarInfo.swift
@@ -0,0 +1,28 @@
+//
+//  ObjCIvarInfo.swift
+//
+//
+//  Created by p-x9 on 2024/06/23
+//  
+//
+
+import Foundation
+import ObjCTypeDecodeKit
+
+public struct ObjCIvarInfo {
+    let name: String
+    let typeEncoding: String
+    let offset: Int
+}
+
+extension ObjCIvarInfo {
+    public var type: ObjCType? {
+        ObjCTypeDecoder.decode(typeEncoding)
+    }
+}
+
+extension ObjCIvarInfo {
+    public var headerString: String {
+        "\(type?.decodedStringForArgument ?? "unknown") \(name);"
+    }
+}
