207c1839d7188f49f6a31b44be41ae912f313830
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-06-28 13:55:15 +0900

    Add test cases

--- Tests/ObjCDumpTests/ObjCDumpTests.swift
+++ Tests/ObjCDumpTests/ObjCDumpTests.swift
@@ -1,4 +1,21 @@
 import XCTest
 @testable import ObjCDump
 
-final class ObjCDumpTests: XCTestCase {}
+final class ObjCDumpTests: XCTestCase {
+    func testClass() {
+        guard let info = classInfo(for: ObjCDumpTests.self) else {
+            XCTAssert(true)
+            return
+        }
+        print(info.headerString)
+    }
+
+    func testProtocol() {
+        guard let `protocol` = NSProtocolFromString("NSCoding"),
+              let info = protocolInfo(for: `protocol`) else {
+            XCTAssert(true)
+            return
+        }
+        print(info.headerString)
+    }
+}
